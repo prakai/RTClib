c868cbc7b50a70c24fe2477244335b26ad86141a
prakai <prakai.na@gmail.com>
2023-02-20 23:24:33 +0700

    Revert "Change lostPower status mark bit in Single RAM byte"
    This reverts commit 73d77f82c861f9fcfb96f1b7d9e35faca11138a6.

--- src/RTC_PCF85363A.cpp
+++ src/RTC_PCF85363A.cpp
@@ -89,7 +89,7 @@ bool RTC_PCF85363A::begin(TwoWire *wireInstance)
 /**************************************************************************/
 bool RTC_PCF85363A::lostPower(void)
 {
-  return (bool)!(read_register(PCF85363A_MARK_AS_INITIALIZED_ADDR) & PCF85363A_MARK_AS_INITIALIZED_VALE);
+  return (bool)(read_register(PCF85363A_MARK_AS_INITIALIZED_ADDR) & PCF85363A_MARK_AS_INITIALIZED_VALE);
 }
 
 /**************************************************************************/
@@ -118,7 +118,7 @@ void RTC_PCF85363A::adjust(const DateTime &dt)
 
   // Mark as initialized in RAM
   uint8_t init[2] = {PCF85363A_MARK_AS_INITIALIZED_ADDR,
-                     read_register(PCF85363A_MARK_AS_INITIALIZED_ADDR) | PCF85363A_MARK_AS_INITIALIZED_VALE};
+                     read_register(PCF85363A_CTRL_STOP_EN) | PCF85363A_MARK_AS_INITIALIZED_VALE};
   i2c_dev->write(init, 2);
 }
 
